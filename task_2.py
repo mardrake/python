4b6eaba5c19358289ec89de42812d00b9cc49e95
mardrake <mardrake@mail.ru>
2021-01-19 19:23:56 +0300

    task_1 done

---
+++ task_2.py
@@ -0,0 +1,10 @@
+'''Реализовать функцию, принимающую несколько параметров,
+описывающих данные пользователя: имя, фамилия, год рождения, город проживания, email, телефон.
+Функция должна принимать параметры как именованные аргументы.
+Реализовать вывод данных о пользователе одной строкой.
+'''
+
+def my_func(**kwargs):
+    return kwargs
+
+print(my_func(name=input('Enter Name: '), second_name=input('Enter Second Name: '), year=input('Enter year of birth: '), city=input('Enter Current city: '), email=input('Enter E-mail: '), phone=input('Enter phone number: ')))
