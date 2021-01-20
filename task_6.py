9ff06cbe59e0d35b1c142929943591b4ac7eaba7
mardrake <mardrake@mail.ru>
2021-01-20 09:58:51 +0300

    task_6 done

---
+++ task_6.py
@@ -0,0 +1,13 @@
+'''
+Реализовать функцию int_func(), принимающую слова из маленьких латинских букв и возвращающую их же, но с прописной первой буквой.
+Например, print(int_func(‘text’)) -> Text.
+'''
+n = input()
+t=''.join(n).split()
+n=' '.join(t)
+def int_func():
+  m = n.lower().capitalize()
+
+  return m
+
+print(int_func())
