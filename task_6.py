ac707d75c294ded5551430b93e39f1cdde508f3d
mardrake <mardrake@mail.ru>
2021-01-22 22:37:59 +0300

    task_6 update

--- task_6.py
+++ task_6.py
@@ -3,14 +3,38 @@
 Например, print(int_func(‘text’)) -> Text.
 '''
 n = input()
-t = ''.join(n).split()
-n = ' '.join(t)
-
 
 def int_func():
-    m = n.lower().capitalize()
+    t = n.lower()
+    l = k = 0
+    m = int(len(n))
+    while m != 0:
+        # print(ord(n[m-1]))
+        if (ord(n[m - 1]) - 97) <= 25:
+            l = ord(t[m - 1]) + l - 97
+            k = ord(n[m - 1]) + k - 97
+            # print(l)
+            # print(k)
+            # print(n[m-1])
+            m = m - 1
+        elif (ord(n[m - 1]) - 97) >= 25:
+            print('Присутствует кириллица')
+            k = k - 1
+            break
+        else:
+            k = k - 1
+            m = m - 1
+    if k == l:
+        z = n.title()
+    else:
+        z='Не правильное слово'
 
-    return m
+    return z
 
 
 print(int_func())
+
+'''
+
+
+'''
