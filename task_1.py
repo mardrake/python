ef846cba4324af20f38e8c79c696745bd1811452
mardrake <mardrake@mail.ru>
2021-01-23 22:16:42 +0300

    task_1 done

--- task_1.py
+++ task_1.py
@@ -1,7 +1,52 @@
-try:
-    n = input('Введите еще числа, через пробел или введите q: ').split()
-    for i, item in enumerate(n):
-        a.append(int(item))
-    print('Сумма: ', sum(a))
-except ValueError:
-    print('Сумма: ', sum(a))
+from sys import argv
+
+script_name, virab, stavka, premiya = argv
+
+
+def itog():
+    while True:
+        try:
+            v = int(virab)
+            s = int(stavka)
+            p = int(premiya)
+            if v > 0 and s > 0 and p > 0:
+                itogo = (v * s) + p
+                print('Все круто, поздравляю с премией!')
+                break
+            elif v <= 0 and s > 0 and p > 0:
+                print('Вы ошиблись Выработка должна быть, ставлю минималку час : ')
+                itogo = s + p
+                break
+            elif v > 0 and s <= 0 and p > 0:
+                print('Считаю по минимальной ставке 2 rub : ')
+                itogo = (v * 2) + p
+                break
+            elif v > 0 and s > 0 and p < 0:
+                print('И как мы отнимем премию? : ')
+                itogo = v * s
+                break
+            elif v > 0 and s > 0 and p == 0:
+                print('Ничего страшного премия будет ещё: ')
+                itogo = v * s
+                break
+            elif v <= 0 and s > 0 and p <= 0:
+                print('Вы ошиблись Выработка должна быть, ставлю минималку час : ')
+                itogo = v * s
+                break
+            else:
+                print('Не надо мудрить!!!')
+                itogo = 0
+                break
+
+        except ValueError:
+            print('Не надо мудрить!!!')
+            itogo = 0
+            break
+    return itogo
+
+
+print("Имя скрипта: ", script_name)
+print("Выработка в часах: ", virab)
+print("Ставка в час: ", stavka)
+print("Премия : ", premiya)
+print("Итого : ", itog(), 'rub')
