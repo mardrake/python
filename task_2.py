3e49a65e5bc0611dfb12bb5d3a7cca511230c777
mardrake <mardrake@mail.ru>
2021-01-24 14:21:16 +0300

    task_2 update

--- task_2.py
+++ task_2.py
@@ -6,3 +6,10 @@ n = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 a = 0
 new_list = [n[a] for a in range(1, len(n)) if n[a-1] < n[a]]
 print(new_list)
+
+m = input().split()
+n = [int(item) for item in m]
+new_list = [n[a] for a in range(1, len(n)) if n[a-1] < n[a]]
+print(new_list)
+#[12, 44, 4, 10, 78, 123]
+#[300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
