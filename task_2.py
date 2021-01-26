cfb1b674242cc0053aa97ce82bb2dd1aef45f778
mardrake <mardrake@mail.ru>
2021-01-26 20:32:18 +0300

    task_2 done

--- task_2.py
+++ task_2.py
@@ -3,8 +3,10 @@
 Об окончании ввода данных свидетельствует пустая строка.
 '''
 stroka = 0
+t = 0
 with open("some.txt", 'r', encoding='utf8') as my_file:
     for line in my_file:
         stroka = stroka + 1
-        print(stroka, 'строка', len(line) -1, 'слов')
+        print(stroka, ':строка!', len(line) - 1, ':слов!')
+
 print('Количество строк в файле = ', stroka)
