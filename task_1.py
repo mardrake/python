2efd52d27b65ce95a34948e9311e0222063b1b60
mardrake <mardrake@mail.ru>
2021-01-27 11:58:56 +0300

    task_1 update

--- task_1.py
+++ task_1.py
@@ -3,11 +3,11 @@
 Об окончании ввода данных свидетельствует пустая строка.
 '''
 
-my_file = open("some.txt", 'w')
-while True:
-    text = input()
-    m = ''.join(text)
-    if not text:
-        break
-    else:
-        my_file.write(f"{m}\n")
+with open("my_file.txt", 'w', encoding='utf-8') as my_file:
+    while True:
+        text = input()
+        m = ''.join(text)
+        if not text:
+            break
+        else:
+            my_file.write(f"{m}\n")
